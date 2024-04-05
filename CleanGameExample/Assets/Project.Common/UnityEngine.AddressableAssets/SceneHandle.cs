12fefecc3b66b9c59c6d92a884e6ec190e02fbd0
Denis <dddenisss35@gmail.com>
2024-04-05 09:16:26 +0300

    SceneHandle

--- CleanGameExample/Assets/Project.Common/UnityEngine.AddressableAssets/SceneHandle.cs
+++ CleanGameExample/Assets/Project.Common/UnityEngine.AddressableAssets/SceneHandle.cs
@@ -18,40 +18,44 @@ namespace UnityEngine.AddressableAssets {
         public bool IsValid => handle!.Value.IsValid();
         public bool IsSucceeded => handle!.Value.IsSucceeded();
         public bool IsFailed => handle!.Value.IsFailed();
-        public SceneInstance SceneInstance => handle!.Value.Result;
-        public Scene Scene => handle!.Value.Result.Scene;
+        public Scene Scene => handle!.Value.Task.Result.Scene;
+        public Task<Scene> SceneAsync => handle!.Value.Task.ContinueWith( i => i.Result.Scene );
 
+        // Constructor
         public SceneHandle(string key) {
             this.key = key;
         }
 
-        public async Task<Scene> LoadAsync(LoadSceneMode mode) {
+        // Load
+        public Task<Scene> LoadAsync(LoadSceneMode mode) {
             Assert.Operation.Message( $"SceneHandle {this} must be non-active" ).Valid( handle == null );
             handle = Addressables.LoadSceneAsync( key, mode, false );
             if (handle.Value.IsValid() && handle.Value.IsFailed()) {
                 throw handle.Value.OperationException;
             }
-            var sceneInstance = await handle.Value.Task;
-            if (handle.Value.IsValid() && handle.Value.IsFailed()) {
-                throw handle.Value.OperationException;
-            }
-            await sceneInstance.ActivateAsync();
-            return sceneInstance.Scene;
+            return handle.Value.Task.ContinueWith( async task => {
+                var sceneInstance = task.Result;
+                if (sceneInstance.Scene.IsValid()) {
+                    await sceneInstance.ActivateAsync();
+                }
+                return sceneInstance.Scene;
+            }, TaskScheduler.FromCurrentSynchronizationContext() ).Unwrap();
         }
+
+        // Unload
         public async Task UnloadAsync() {
             Assert.Operation.Message( $"SceneHandle {this} must be active" ).Valid( handle != null );
-            var tmp_handle = handle;
+            await Addressables.UnloadSceneAsync( handle.Value ).Task;
             handle = null;
-            await Addressables.UnloadSceneAsync( tmp_handle.Value ).Task;
         }
         public async Task SafeUnloadAsync() {
             if (handle != null) {
-                var tmp_handle = handle;
+                await Addressables.UnloadSceneAsync( handle.Value ).Task;
                 handle = null;
-                await Addressables.UnloadSceneAsync( tmp_handle.Value ).Task;
             }
         }
 
+        // Utils
         public override string ToString() {
             return $"SceneHandle: {key}";
         }

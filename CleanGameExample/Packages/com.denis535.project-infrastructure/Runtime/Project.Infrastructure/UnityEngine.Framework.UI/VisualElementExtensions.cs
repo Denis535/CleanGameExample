aacedd376a796b5045c40f64f2f77df696a3e976
Denis <dddenisss35@gmail.com>
2024-09-08 19:35:47 +0300

    Refactored

--- CleanGameExample/Packages/com.denis535.project-infrastructure/Runtime/Project.Infrastructure/UnityEngine.Framework.UI/VisualElementExtensions.cs
+++ CleanGameExample/Packages/com.denis535.project-infrastructure/Runtime/Project.Infrastructure/UnityEngine.Framework.UI/VisualElementExtensions.cs
@@ -7,6 +7,33 @@ namespace UnityEngine.Framework.UI {
     using UnityEngine;
     using UnityEngine.UIElements;
 
+    public static class VisualElementExtensions {
+
+        public static void SetValue<T>(this BaseField<T> element, T value) {
+            element.value = value;
+        }
+        public static void SetValue<T>(this PopupField<T> element, T value, List<T> choices) {
+            (element.value, element.choices) = (value, choices.ToList());
+        }
+        public static void SetValue<T>(this PopupField<T> element, T value, T[] choices) {
+            (element.value, element.choices) = (value, choices.ToList());
+        }
+        public static void SetValue<T>(this BaseSlider<T> element, T value, T min, T max) where T : IComparable<T> {
+            (element.value, element.lowValue, element.highValue) = (value, min, max);
+        }
+
+        public static void OnValidate(this VisualElement element, EventCallback<EventBase> callback, TrickleDown useTrickleDown = TrickleDown.NoTrickleDown) {
+            // todo: how to handle any event?
+            //element.RegisterCallback<EventBase>( callback, useTrickleDown );
+            element.RegisterCallback<AttachToPanelEvent>( callback, useTrickleDown );
+            element.RegisterCallback<ChangeEvent<object?>>( callback, useTrickleDown );
+            element.RegisterCallback<ChangeEvent<string?>>( callback, useTrickleDown );
+            element.RegisterCallback<ChangeEvent<int>>( callback, useTrickleDown );
+            element.RegisterCallback<ChangeEvent<float>>( callback, useTrickleDown );
+            element.RegisterCallback<ChangeEvent<bool>>( callback, useTrickleDown );
+        }
+
+    }
     public static partial class VisualElementFactory {
 
         public static Func<object?, string?>? StringSelector { get; set; }

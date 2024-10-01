b3c82050a330e73e9fb28d87392090c0721969a7
Denis <dddenisss35@gmail.com>
2024-10-01 10:05:05 +0300

    ProjectWindow

--- UnityShooterExample/Assets/Project.Content/Project.00/Editor/ProjectWindow2.cs
+++ UnityShooterExample/Assets/Project.Content/Project.00/Editor/ProjectWindow2.cs
@@ -28,7 +28,7 @@ namespace Project {
             base.DrawPackage( rect, path, package, content );
         }
         protected override void DrawAssembly(Rect rect, string path, string assembly, string content) {
-            if (Path.GetExtension( path ) is not (".asmdef" or ".asmref")) {
+            if (Path.GetExtension( path ) is not (".asmdef" or ".asmref" or ".rsp")) {
                 base.DrawAssembly( rect, path, assembly, content );
             }
         }
@@ -36,12 +36,12 @@ namespace Project {
         // Helpers
         private static string[] GetPackagePaths() {
             return Enumerable.Empty<string>()
+                .Append( "Packages/com.denis535.project-infrastructure" )
                 .Append( "Packages/com.denis535.clean-architecture-game-framework" )
-                .Append( "Packages/com.denis535.uitoolkit-theme-style-sheet" )
                 .Append( "Packages/com.denis535.addressables-extensions" )
                 .Append( "Packages/com.denis535.addressables-source-generator" )
                 .Append( "Packages/com.denis535.colorful-project-window" )
-                .Append( "Packages/com.denis535.project-infrastructure" )
+                .Append( "Packages/com.denis535.uitoolkit-theme-style-sheet" )
                 .ToArray();
         }
         private static string[] GetModulePaths() {
